a966af5233e17452387c18ca5206a0e6c8ec81ce
Arthur <Warriorroq@gmail.com>
2022-04-22 00:42:30 +0200

    added some options/effects on start

--- Maze_v3/Game.cpp
+++ Maze_v3/Game.cpp
@@ -34,14 +34,28 @@ void Game::Update(char key) {
 void Game::CreateEntities() {
     auto mainCamera = new Camera(Vector2Int(100, 18), Vector2Int());
     _DrawMatrix = new Field(mainCamera, ' ');
+    cout << "Creating entities..." << endl;
+    this_thread::sleep_for(chrono::milliseconds(1500));
     for (int i = 0; i < 20000; i++)
         ConnectEntityToGameCycle(new Entity(Vector2Int(rand() % 300 + 49, -(rand() % 300 + 8)), '!'));
     auto player = new Player(Vector2Int(rand() % 50 + 100, -(rand() % 9 + 30)), _DrawMatrix, mainCamera);
     auto exit = new Entity(Vector2Int(rand() % 50 + 49, -(rand() % 9 + 8)), 'E');
     ConnectEntityToGameCycle(player);
     ConnectEntityToGameCycle(exit);
-    ConnectEntityToGameCycle(new Fog(10, '#'));
-    ConnectEntityToGameCycle(new Compass(player, exit, '-'));
+    cout << "Add fog? y/n"<<endl;
+    char answerForFog;
+    cin >> answerForFog;
+    if (answerForFog == 'y') {
+        cout << "View range? reccomend(7 - 12) y/n" << endl;
+        int viewRange;
+        cin >> viewRange;
+        ConnectEntityToGameCycle(new Fog(viewRange, '#'));
+        ConnectEntityToGameCycle(new Compass(player, exit, '-'));
+        cout << "Game will start in a few seconds..."<<endl;
+        this_thread::sleep_for(chrono::milliseconds(1500));
+        cout << "We added compass to the left top..."<<endl;
+        this_thread::sleep_for(chrono::milliseconds(1500));
+    }
 }
 void Game::ConnectEntityToGameCycle(Entity* entity) {
     _Drawables.push_back(entity);
